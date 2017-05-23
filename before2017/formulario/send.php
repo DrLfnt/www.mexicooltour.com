b8e3475fbce8cade04c206cce16fdc14682fef86
Argenis Oldair Palacios Escobar A.K.A. Dr. Lfnt <Dr.Lfnt@gmail.com>
2017-05-22 19:44:13 -0500

    Ajuste de organizacion
    se organizó todo y se crearon nuevas secciones de la version 2017

---
+++ before2017/formulario/send.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_POST["nombre"]) && isset($_POST["email"]) && isset($_POST["comentario"]) ){
+$to = "jorge@mexicooltour.com";
+$subject = "Mensaje Enviado";
+$contenido .= "Nombre: ".$_POST["nombre"]."\n";
+$contenido .= "Email: ".$_POST["email"]."\n\n";
+$contenido .= "Comentario: ".$_POST["comentario"]."\n\n";
+$header = "From: jorge@mexicooltour.com\nReply-To:".$_POST["email"]."\n";
+$header .= "Mime-Version: 1.0\n";
+$header .= "Content-Type: text/plain";
+if(mail($to, $subject, $contenido ,$header)){
+echo "Mail Enviado.";
+}
+}
+?>
